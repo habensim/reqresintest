91d9018146f0698e982c0a65ef6a1bd9cb1377bd
unknown <habensimanjuntak>
2021-10-26 17:27:21 +0700

    add Test for flip.id test

---
+++ test/reqrestest.js
@@ -0,0 +1,33 @@
+import supertest from "supertest";
+
+const request = supertest('https://reqres.in');
+
+import { expect } from "chai";
+
+describe('Testing Reqres', () => {
+    it('GET /api/users', () => {
+        return request.get(`/api/users?page=2`).then((res) => {
+            expect(res.body.data).to.not.be.empty;
+        });
+    });
+
+    it('GET /api/users/:id', () => {
+        return request.get(`/api/users/2`).then((res) => {
+            expect(res.body.data.id).to.be.eq(2);
+        });
+    });
+
+    it('POST api/users', () => {
+        const data = {
+            "name": "morpheus",
+            "job": "leader"
+        };
+        return request
+            .post('/api/users')
+            // .set('Authorization', `Bearer ${TOKEN}`)
+            .send(data)
+            .then((res) => {
+                console.log(res.body);
+            });
+    });
+});
